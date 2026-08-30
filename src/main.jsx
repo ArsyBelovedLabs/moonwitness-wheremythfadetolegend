c765a4e2ec3eb814e798fb043d818362a4deb2c6
bjo163 <33343513+bjo163@users.noreply.github.com>
2026-08-30 23:27:00 +0700

    Mount live monitoring widget in application shell

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import MonitorWidget from './components/MonitorWidget.jsx'
 import './shadcn.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <div className="pointer-events-none fixed inset-x-0 bottom-4 z-40 px-4 sm:px-6">
+      <div className="pointer-events-auto mx-auto max-w-2xl">
+        <MonitorWidget />
+      </div>
+    </div>
   </React.StrictMode>
 )
