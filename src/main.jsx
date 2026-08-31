e12c6eb015cfeec93cc4ffcf448cf9f8ce5b4be9
bjo163 <33343513+bjo163@users.noreply.github.com>
2026-08-31 07:44:41 +0700

    feat: wrap myth research UI with shared design system

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { MoonWitnessProvider } from "@arsybelovedlabs/moonwitness-design-system"
+import "@arsybelovedlabs/moonwitness-design-system/styles.css"
 import App from "./ResearchInstrument.jsx"
 import "./where-myth-dashboard.css"
 import "./research-instrument.css"
+import "./myth-design-system.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <MoonWitnessProvider theme="myth-fade">
+      <App />
+    </MoonWitnessProvider>
   </React.StrictMode>,
 )
