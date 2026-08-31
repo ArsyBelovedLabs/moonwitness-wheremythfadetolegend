1cc5738844c8a9c994d31c2c110eededb0b41fc1
bjo163 <33343513+bjo163@users.noreply.github.com>
2026-08-31 08:20:26 +0700

    feat: consume shared MoonWitness application shell

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { MoonWitnessProvider } from "@arsybelovedlabs/moonwitness-design-system"
+import { ApplicationShell, MoonWitnessProvider } from "@arsybelovedlabs/moonwitness-design-system"
 import "@arsybelovedlabs/moonwitness-design-system/styles.css"
 import App from "./ResearchInstrument.jsx"
 import "./where-myth-dashboard.css"
@@ -10,7 +10,9 @@ import "./myth-design-system.css"
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <MoonWitnessProvider theme="myth-fade">
-      <App />
+      <ApplicationShell label="WHERE MYTH FADE TO LEGEND" eyebrow="MOONWITNESS / COUNTER-MYTHOS OBSERVATORY">
+        <App />
+      </ApplicationShell>
     </MoonWitnessProvider>
   </React.StrictMode>,
 )
